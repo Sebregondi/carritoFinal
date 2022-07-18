4bc8f75fef495b3ea7c35fc256b8583dbd007064
Esteban Landucci <emlanducci@gmail.com>
2022-07-18 13:44:05 -0300

    se agregó stock.js para luego convertirlo a json

---
+++ getData.js
@@ -0,0 +1,9 @@
+const productos = async () => {
+    const resp = await fetch('./data.json')
+    const data = await resp.json()
+
+    console.log(productos)
+}
+
+export {productos};
+console.log (productos)
