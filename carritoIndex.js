e0b935e5ca83774d89e811f5439245ed97c52135
Esteban Landucci <emlanducci@gmail.com>
2022-07-18 14:28:02 -0300

    carritoIndex listo

--- carritoIndex.js
+++ carritoIndex.js
@@ -28,5 +28,45 @@ const agregarProductoAlCarrito = (productoID, carritoDeCompras) => {
 
     producto.cantidad = 1;
 
-    const
+    const div = document.createElement ('div');
+    div.classList.add ('productoEnCarrito');
+
+    div.innerHTML = `<p>${producto.nombre}</p>
+                    <p>Precio: ${producto.precio}</p>
+                    <p id=cantidad${producto.id}>Cantidad:${producto.cantidad}</p>
+                    <button id=eliminar${producto.id}
+                    class="btn waves-effect waves-light boton-eliminar" value ="${producto.id}">X</i></button>`
+
+    contenedor.appendChild (div);
+    actualizarCarrito (carritoDeCompras);
+};
+
+export const eliminarProductosCarrito = (productoId) => {
+    const carritoStorage = obtenerCarritoStorage();
+    const carritoActualizado = carritoStorage.filter (el.id != productoId);
+
+    actualizarCarrito (carritoActualizado);
+    pintarProductosCarrito (carritoActualizado);
+};
+
+export const pintarProductosCarrito = (carritoDeCompras) => {
+    const contenedor = document.getElementById ('carrito-contenedor');
+
+    contenedor.innerHTML = "";
+
+    carritoDeCompras.forEach(producto => {
+        const div = document.createElement('div');
+        div.classList.add('productoEnCarrito');
+        div.innerHTML = `<p>${producto.nombre}</p>
+                        <p>Precio:${producto.precio}</p>
+                        <p id=cantidad${producto.id}>Cantidad:${producto.cantidad}</p>
+                        <button id=eliminar${producto.id} class="btn waves-effect waves-light boton-eliminar" value="${producto.id}">X</button>`
+
+        contenedor.appendChild(div);
+    });
+};
+
+export const obtenerCarritoStorage = () => {
+    const carritoStorage = JSON.parse (localStorage.getItem ("carrito"))
+    return carritoStorage;
 }
