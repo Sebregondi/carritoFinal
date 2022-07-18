a0ceba9746e6f954f6e1feef7a0a30f09bb191ab
Esteban Landucci <emlanducci@gmail.com>
2022-07-18 13:23:35 -0300

    se agregó export

--- actualizarCarrito.js
+++ actualizarCarrito.js
@@ -1,7 +1,7 @@
 const contadorCarrito = document.getElementById ('contador-carrito');
 const precioTotal = document.getElementById ('precioTotal');
 
-const actualizarCarrito = (carritoDeCompras) => {
+export const actualizarCarrito = (carritoDeCompras) => {
     contadorCarrito.innerText = carritoDeCompras.reduce((acc, el) => acc + el.cantidad, 0);
     precioTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + (el.precio * el.cantidad), 0);
     localStorage.setItem ("carrito", JSON.stringify (carritoDeCompras));
